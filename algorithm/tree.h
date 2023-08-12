16d4a0d6e0b4275996a2bbd8376c61feab53b5ee
chenokay <chenokay@gmail.com>
2023-08-12 19:00:33 +0800

    print node of binary tree

--- algorithm/tree.h
+++ algorithm/tree.h
@@ -15,6 +15,7 @@ void trace_tree_v1(tree * n)
 	}
 }
 
+// 中序
 void trace_tree_v2(tree * n)
 {
 
@@ -32,4 +33,54 @@ void trace_tree_v2(tree * n)
 			p = p->right;
 		}
 	}
+}
+
+// 后序
+void trace_tree_v2(tree * n)
+{
+
+	Stack s;
+	tree * p = n;
+    tree* last_p = NULL;
+
+	for (;!s.empty() || NULL != p;) {
+
+		if(NULL != p) {
+			s.push(p);		
+			p = p->left;
+		} else if(!s.empty()) {
+			p=s.top();
+            if (NULL != p->right && last_p != p->right){
+                p = p->right;
+                continue;
+            }
+			p->print();
+            last_p = p;
+            p.pop();
+		}
+	}
+}
+
+// 前序
+void trace_tree_v2(tree * n)
+{
+
+	Stack s;
+	tree * p = n;
+    s.push(p);
+
+	for (;!s.empty() || NULL != p;) {
+
+        p = s.pop();
+
+        if(NULL != p) {
+            p->print();
+        }
+        if (NULL != p->right) {
+            s.push(p->rhght);
+        }
+        if (NULL != p->left) {
+            s.push(p->left);
+        }
+    }
 }
